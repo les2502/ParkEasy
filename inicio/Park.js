6a198ce6662d29629c942d5e2899223e0afd9b99
maryu1605 <163140229+maryu1605@users.noreply.github.com>
2024-05-07 08:37:18 -0500

    Update Park.js

--- inicio/Park.js
+++ inicio/Park.js
@@ -1,5 +1,7 @@
-fuction sendMail(){
-  let parms={
-    
-  }
-}
+// Iniciar Sesión
+var provider = new firebase.auth.GoogleAuthProvider();
+
+$('')
+firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
